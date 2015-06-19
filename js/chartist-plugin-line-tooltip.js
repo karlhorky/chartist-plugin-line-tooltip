bbc2b62f68a114bfe53692c4d39e6076bed5539b
Karl Horky <karl.horky@gmail.com>
2015-06-19 16:31:52 +0200

    Add mouse event listeners to hover areas

--- js/chartist-plugin-line-tooltip.js
+++ js/chartist-plugin-line-tooltip.js
@@ -11,6 +11,50 @@
   var defaultOptions = {
   };
 
+  // var arrayify = function(nodelist) {
+  //   return Array.prototype.slice.call(nodelist);
+  // };
+
+  // var createHoverArea = function(width) {
+  //   // TODO: Don't initialize every time
+  //   var hoverAreaWidth, x, hoverArea, hoverAreaNode;
+
+  //   x              = Math.floor(point.x - (width / 2));
+  //   hoverAreaWidth = Math.ceil(width);
+
+  //   if (x < 0) {
+  //     hoverAreaWidth += x;
+  //     x = 0;
+  //   }
+
+  //   hoverArea = point.group.elem('rect', {
+  //     x     : x,
+  //     y     : 0,
+  //     width : hoverAreaWidth,
+  //     height: '100%',
+  //     fill  : 'transparent',
+  //     index : point.index
+  //   }, 'ct-point-hover-area');
+
+  //   hoverAreaNode = hoverArea._node;
+
+  //   var attach = attachTooltip.bind(null, point);
+  //   var detach = detachTooltip.bind(null, point);
+  //   hoverAreaNode.addEventListener('mouseenter', attach);
+  //   hoverAreaNode.addEventListener('mouseleave', detach);
+  // };
+
+  var attachTooltip = function(point, e) {
+    console.log(point);
+  // console.log(e.currentTarget.dataset.point);
+  };
+
+  var detachTooltip = function(point, e) {
+    console.log(point);
+    // console.log(e.currentTarget);
+  };
+
+
   Chartist.plugins = Chartist.plugins || {};
 
   Chartist.plugins.lineTooltip = function(options) {
@@ -22,35 +66,48 @@
       var points = [];
 
       chart.on('draw', function(data) {
-        // console.log('data', data)
         if (data.type === 'point') {
           points.push(data);
         }
       });
 
       chart.on('created', function() {
-        // console.log(arguments)
         if (points.length < 2) { return; }
 
         var width = (points[1].x - points[0].x),
-            rectWidth,
-            x;
+            hoverAreaWidth,
+            x,
+            hoverArea,
+            hoverAreaNode;
 
-        points.forEach(function(point) {
-          x = point.x - (width / 2);
-          rectWidth = width;
+        points.forEach(function(point, i) {
+          // createHoverArea(width);
+          x              = Math.floor(point.x - (width / 2));
+          hoverAreaWidth = Math.ceil(width);
 
           if (x < 0) {
-            rectWidth += x;
+            hoverAreaWidth += x;
             x = 0;
           }
 
-          point.group.elem('rect', {
+          hoverArea = point.group.elem('rect', {
             x     : x,
             y     : 0,
-            width : rectWidth,
-            height: '100%'
-          },'ct-label');
+            width : hoverAreaWidth,
+            height: '100%',
+            fill  : 'transparent',
+            index : point.index
+          }, 'ct-point-hover-area');
+
+          hoverAreaNode = hoverArea._node;
+
+          var attach = attachTooltip.bind(null, point);
+          var detach = detachTooltip.bind(null, point);
+          hoverAreaNode.addEventListener('mouseenter', attach);
+          hoverAreaNode.addEventListener('mouseleave', detach);
+
+          // Create tooltips
+
         });
       });
     };
