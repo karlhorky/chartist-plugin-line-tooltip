bd7bd6347b69bb923fb49b3ec9ebd61e011b2e84
Karl Horky <karl.horky@gmail.com>
2015-06-19 18:28:48 +0200

    Break out code into functions

--- js/chartist-plugin-line-tooltip.js
+++ js/chartist-plugin-line-tooltip.js
@@ -15,44 +15,7 @@
   //   return Array.prototype.slice.call(nodelist);
   // };
 
-  // var createHoverArea = function(width) {
-  //   // TODO: Don't initialize every time
-  //   var hoverAreaWidth, x, hoverArea, hoverAreaNode;
 
-  //   x              = Math.floor(point.x - (width / 2));
-  //   hoverAreaWidth = Math.ceil(width);
-
-  //   if (x < 0) {
-  //     hoverAreaWidth += x;
-  //     x = 0;
-  //   }
-
-  //   hoverArea = point.group.elem('rect', {
-  //     x     : x,
-  //     y     : 0,
-  //     width : hoverAreaWidth,
-  //     height: '100%',
-  //     fill  : 'transparent',
-  //     index : point.index
-  //   }, 'ct-point-hover-area');
-
-  //   hoverAreaNode = hoverArea._node;
-
-  //   var attach = attachTooltip.bind(null, point);
-  //   var detach = detachTooltip.bind(null, point);
-  //   hoverAreaNode.addEventListener('mouseenter', attach);
-  //   hoverAreaNode.addEventListener('mouseleave', detach);
-  // };
-
-  var attachTooltip = function(point, e) {
-    console.log(point);
-  // console.log(e.currentTarget.dataset.point);
-  };
-
-  var detachTooltip = function(point, e) {
-    console.log(point);
-    // console.log(e.currentTarget);
-  };
 
 
   Chartist.plugins = Chartist.plugins || {};
@@ -60,10 +23,49 @@
   Chartist.plugins.lineTooltip = function(options) {
     options = Chartist.extend({}, defaultOptions, options);
 
-    return function tooltip(chart) {
+    return function plugin(chart) {
       if (!(chart instanceof Chartist.Line)) { return; }
 
       var points = [];
+
+      var attachTooltip = function(point, e) {
+        console.log(point);
+      // console.log(e.currentTarget.dataset.point);
+      };
+
+      var detachTooltip = function(point, e) {
+        console.log(point);
+        // console.log(e.currentTarget);
+      };
+
+      var createHoverArea = function(points, point, width) {
+        // TODO: Don't initialize every time
+        var hoverAreaWidth, x, hoverArea, hoverAreaNode;
+
+        x              = Math.floor(point.x - (width / 2));
+        hoverAreaWidth = Math.ceil(width);
+
+        if (x < 0) {
+          hoverAreaWidth += x;
+          x = 0;
+        }
+
+        hoverArea = point.group.elem('rect', {
+          x     : x,
+          y     : 0,
+          width : hoverAreaWidth,
+          height: '100%',
+          fill  : 'transparent',
+          index : point.index
+        }, 'ct-point-hover-area');
+
+        hoverAreaNode = hoverArea._node;
+
+        var attach = attachTooltip.bind(null, point);
+        var detach = detachTooltip.bind(null, point);
+        hoverAreaNode.addEventListener('mouseenter', attach);
+        hoverAreaNode.addEventListener('mouseleave', detach);
+      };
 
       chart.on('draw', function(data) {
         if (data.type === 'point') {
@@ -74,37 +76,13 @@
       chart.on('created', function() {
         if (points.length < 2) { return; }
 
-        var width = (points[1].x - points[0].x),
-            hoverAreaWidth,
+        var width = points[1].x - points[0].x,
             x,
             hoverArea,
             hoverAreaNode;
 
         points.forEach(function(point, i) {
-          // createHoverArea(width);
-          x              = Math.floor(point.x - (width / 2));
-          hoverAreaWidth = Math.ceil(width);
-
-          if (x < 0) {
-            hoverAreaWidth += x;
-            x = 0;
-          }
-
-          hoverArea = point.group.elem('rect', {
-            x     : x,
-            y     : 0,
-            width : hoverAreaWidth,
-            height: '100%',
-            fill  : 'transparent',
-            index : point.index
-          }, 'ct-point-hover-area');
-
-          hoverAreaNode = hoverArea._node;
-
-          var attach = attachTooltip.bind(null, point);
-          var detach = detachTooltip.bind(null, point);
-          hoverAreaNode.addEventListener('mouseenter', attach);
-          hoverAreaNode.addEventListener('mouseleave', detach);
+          createHoverArea(points, point, width);
 
           // Create tooltips
 
