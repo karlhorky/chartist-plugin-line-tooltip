b4be9a083fc3b93879c33b8568d66f97fb399454
Karl Horky <karl.horky@gmail.com>
2015-07-13 00:19:10 +0200

    Improve tooltip creation, positioning and styling

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # chartist-plugin-line-tooltip
 Plugin for Chartist.js Line Charts
+
+*In development...*
+
+## Why another tooltip plugin?
+
+- No jQuery dependency
+- Tooltips fixed to points instead of mouse
+- Multiple series values in one tooltip
+- Hover targets cover the full chart instead of just the points
--- js/chartist-plugin-line-tooltip.js
+++ js/chartist-plugin-line-tooltip.js
@@ -27,134 +27,61 @@
       var
         points   = [],
         tooltips = [],
-        css      = '';
+        hoverEls = [],
+        tooltipStyles;
 
-      var showTooltip = function (tooltip, e) {
-        tooltip.style.opacity = 1;
-        // console.log(e.currentTarget.dataset.point);
-      };
+      tooltipStyles =
+        '.ct-tooltip-hover {' +
+        'position: absolute;' +
+        'z-index: 10000;' +
+        '}' +
+        '.ct-tooltip {' +
+        'position: absolute;' +
+        'display: inline-block;' +
+        'min-width: 5em;' +
+        'padding: .4em .75em;' +
+        'background: #f4c63d;' +
+        'color: #453d3f;' +
+        'z-index: 1;' +
+        'opacity: 0;' +
+        '-webkit-transition: opacity .15s;' +
+        '-o-transition: opacity .15s;' +
+        'transition: opacity .15s;' +
+        '}' +
+        '.ct-tooltip-hover:hover + .ct-tooltip {' +
+        'opacity: 1;' +
+        '}' +
+        '.ct-tooltip:after {' +
+        'content: "";' +
+        'position: absolute;' +
+        'top: 100%;' +
+        'left: 50%;' +
+        'width: 0;' +
+        'height: 0;' +
+        'margin-left: -15px;' +
+        'border: 15px solid transparent;' +
+        'border-top-color: #f4c63d;' +
+        '}' +
+        '.ct-tooltip-legend,' +
+        '.ct-tooltip-caption-value {' +
+        'vertical-align: middle;' +
+        '}' +
+        '.ct-tooltip-legend {' +
+        'display: inline-block;' +
+        'width: 6px;' +
+        'height: 6px;' +
+        'margin-right: 4px;' +
+        'border: 1px solid #453d3f;' +
+        'border-radius: 2px;' +
+        '}' +
+        '.ct-tooltip-legend-a {' +
+        'background: #d70206;' +
+        '}' +
+        '.ct-tooltip-legend-b {' +
+        'background: #f05b4f;' +
+        '}';
 
-      var hideTooltip = function (tooltip, e) {
-        console.log(e);
-        if (e.toElement)
-        tooltip.style.opacity = 0;
-        // console.log(e.currentTarget);
-      };
-
-      var createHoverArea = function (point, width) {
-
-
-
-        // TODO: add with HTML (easier hovering with just CSS: .hover-area:hover + .tooltip, .tooltip:hover)
-
-
-
-
-        var x = Math.floor(point.x - (width / 2));
-        width = Math.ceil(width);
-
-        if (x < 0) {
-          width += x;
-          x = 0;
-        }
-
-        var hoverArea = point.group.elem('rect', {
-          x     : x,
-          y     : 0,
-          width : width,
-          height: '100%',
-          fill  : 'transparent',
-          index : point.index
-        }, 'ct-point-hover-area');
-
-        return hoverArea;
-      };
-
-      var createTooltip = function (tooltip, container) {
-
-        var tooltipEl = document.createElement('div');
-        tooltipEl.innerHTML = tooltip.content;
-
-        // TODO: Set this CSS in the stylesheet
-        tooltipEl.setAttribute('style', 'position:absolute;height:80px;width:120px;background:red;transition:opacity .15s;');
-        tooltipEl.setAttribute('class', 'ct-point-tooltip');
-        tooltipEl.setAttribute('id', 'ct-point-tooltip-' + tooltip.id);
-
-        container.appendChild(tooltipEl);
-        // var tooltip = point.group.elem('rect', {
-        //   x     : point.x,
-        //   y     : point.y,
-        //   width : 100,
-        //   height: 50,
-        //   fill  : 'red',
-        //   index : point.index
-        // }, 'ct-point-tooltip');
-
-        return tooltipEl;
-      };
-
-      var getTooltipContents = function (seriesArray) {
-        var
-          contents = [],
-          legends = [],
-          series;
-
-        if (seriesArray.length > 1) {
-          legends = seriesArray.map(function (series, i) {
-            return '<span id="' + i + '">' + i + ':</span>';
-          });
-        }
-
-
-        seriesArray.forEach(function(series, s) {
-          var legend = legends[s] || '';
-
-          series.data.forEach(function (value, i) {
-            contents[i] = (contents[i] || '') + '<div>'+legend+value+'</div>';
-          });
-        });
-
-        return contents;
-      };
-
-      var createTooltips = function (data) {
-        var container = document.createElement('div');
-        container.setAttribute('class', 'ct-tooltips');
-        document.querySelector('body').appendChild(container);
-
-        var tooltipContents = getTooltipContents(data.data.series);
-
-        tooltipContents.forEach(function (content, i) {
-          tooltips[i] = createTooltip({
-            id     : i,
-            content: content
-          }, container);
-        });
-      };
-
-      var setTooltipPosition = function (tooltip, point, previousPosition) {
-        var position = point.element._node.getBoundingClientRect();
-        var left = position.left;
-        var top  = position.top;
-
-        if (previousPosition && previousPosition.top && previousPosition.top < top) {
-          top = previousPosition.top;
-        }
-
-        return {
-          id  : tooltip.id,
-          left: left,
-          top : top
-        };
-      };
-
-      var tooltipPositionsCSS = function (tooltipPositions) {
-        return tooltipPositions.map(function (position) {
-          return '#' + position.id + ' { left: ' + position.left + 'px; top: ' + position.top + 'px; }';
-        }).join('\n');
-      };
-
-      var addCSS = function (css) {
+      var addStyles = function (css) {
         var stylesheet = document.createElement('style');
 
         if (stylesheet.styleSheet){
@@ -168,24 +95,133 @@
         return document.querySelector('head').appendChild(stylesheet);
       };
 
-      var showTooltipOnHover = function (hoverArea, tooltip) {
+      var getOffset = function (el) {
+        var docEl = document.documentElement;
+        var boundingRect = el.getBoundingClientRect();
+        var top = boundingRect.top + window.pageYOffset - docEl.clientTop;
+        var left = boundingRect.left + window.pageXOffset - docEl.clientLeft;
+
+        return {
+          top: top,
+          left: left
+        };
+      };
+
+      var getPointOffset = function (point) {
+        return getOffset(point.element._node);
+      };
+
+      var getTooltip = function (options) {
+        var tooltipEl = document.createElement('div');
+        tooltipEl.innerHTML = options.content;
+
+        tooltipEl.setAttribute('class', 'ct-tooltip');
+        tooltipEl.setAttribute('id', 'ct-tooltip-' + options.id);
+
+        return tooltipEl;
+      };
+
+      var getTooltipContents = function (seriesArray) {
         var
-          show          = showTooltip.bind(null, tooltip),
-          hide          = hideTooltip.bind(null, tooltip),
-          hoverAreaNode = hoverArea._node;
+          contents = [],
+          legends = [],
+          series;
 
-        hoverAreaNode.addEventListener('mouseenter', show);
-        hoverAreaNode.addEventListener('mouseleave', hide);
+        if (seriesArray.length > 1) {
+          legends = seriesArray.map(function (series, i) {
+            return '<span class="ct-tooltip-legend ct-tooltip-legend-' + String.fromCharCode(97 + i) + '"></span>';
+          });
+        }
 
-        tooltip.addEventListener('mouseenter', function(e) {
-          console.log('mouseenter tooltip')
-          e.preventDefault();
-          e.stopPropagation();
+        seriesArray.forEach(function(series, s) {
+          var legend = legends[s] || '';
+
+          series.data.forEach(function (value, i) {
+            contents[i] = (contents[i] || '') +
+              '<div class="ct-tooltip-caption-line ct-tooltip-caption-line-' + i + '">' +
+                legend +
+                '<span class="ct-tooltip-caption-value">' +
+                  value +
+                '</span>' +
+              '</div>';
+          });
         });
 
-        // console.log(i);
-        // return '.ct-point-tooltip { opacity: 0; transition: opacity .15s; stroke: 1px solid black; }.ct-point-hover-area:hover + .ct-point-tooltip, .ct-point-tooltip:hover { opacity: 1 }';
+        return contents;
       };
+
+      var getHoverEl = function (options) {
+        var hoverEl = document.createElement('div');
+
+        hoverEl.setAttribute('class', 'ct-tooltip-hover');
+        hoverEl.setAttribute('id', 'ct-tooltip-hover-' + options.id);
+
+        return hoverEl;
+      };
+
+      var createTooltips = function (data) {
+        var container = document.createElement('div');
+        container.setAttribute('class', 'ct-tooltips');
+
+        var
+          hoverEl,
+          tooltip,
+          tooltipContents = getTooltipContents(data.data.series);
+
+        tooltipContents.forEach(function (content, i) {
+          hoverEls[i] = hoverEl = getHoverEl({
+            id: i
+          });
+
+          container.appendChild(hoverEl);
+
+          tooltips[i] = tooltip = getTooltip({
+            id     : i,
+            content: content
+          });
+
+          container.appendChild(tooltip);
+        });
+
+        document.querySelector('body').appendChild(container);
+      };
+
+      var positionHoverEl = function (hoverEl, point, width) {
+        var
+          pointPosition = getPointOffset(point),
+          chartPosition = getOffset(chart.container),
+          height        = chart.container.offsetHeight,
+          left          = Math.floor(pointPosition.left - (width / 2));
+
+        width = Math.ceil(width);
+
+        if (left < chartPosition.left) {
+          width += (left - chartPosition.left);
+          left = chartPosition.left;
+        }
+
+        hoverEl.style.left   = left + 'px';
+        hoverEl.style.top    = chartPosition.top + 'px';
+        hoverEl.style.width  = width + 'px';
+        hoverEl.style.height = height + 'px';
+      };
+
+      var positionTooltip = function (tooltip, point) {
+        var
+          pointPosition = getPointOffset(point),
+          tooltipSize   = {
+            width : tooltip.offsetWidth,
+            height: tooltip.offsetHeight
+          };
+
+        // Arrow
+        tooltipSize.height += 21;
+
+        tooltip.style.left = (pointPosition.left - tooltipSize.width / 2) + 'px';
+        tooltip.style.top  = (pointPosition.top - tooltipSize.height) + 'px';
+      };
+
+      addStyles(tooltipStyles);
 
       chart.on('data', function (data) {
         createTooltips(data);
@@ -193,7 +229,9 @@
 
       chart.on('draw', function (data) {
         if (data.type === 'point') {
-          points.push(data);
+          points[data.index] = points[data.index] || [];
+          points[data.index].push(data);
+          console.log(data);
         }
       });
 
@@ -203,26 +241,19 @@
         if (points.length < 2) {
           width = '100%';
         } else {
-          width = points[1].x - points[0].x;
+          width = points[1][0].x - points[0][0].x;
         }
 
         var tooltipPositions = [];
 
-        css += '.ct-point-tooltip { opacity: 0 } .ct-point-tooltip:hover { opacity: 1 }';
+        points.forEach(function (pointSet) {
+          var point = pointSet.sort(function (a, b) {
+            return b.y - a.y;
+          }).pop();
 
-        points.forEach(function (point) {
-          var hoverArea = createHoverArea(point, width);
-          var tooltip = tooltips[point.index];
-          tooltipPositions[point.index] = setTooltipPosition(tooltip, point, tooltipPositions[point.index]);
-
-          // TODO: set this dynamically - not in a stylesheet
-          css += tooltipPositionsCSS(tooltipPositions);
-
-          // css += setTooltipLegendColor()
-          showTooltipOnHover(hoverArea, tooltip);
+          positionHoverEl(hoverEls[point.index], point, width);
+          positionTooltip(tooltips[point.index], point);
         });
-
-        addCSS(css);
       });
     };
   };
